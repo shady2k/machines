b6e318ce705f0ac52e3483e982cb9e401faef0d6
shady2k <shady2k@gmail.com>
2013-02-04 23:18:28 +0400

    Update machines_filter.lua

--- machines_filter.lua
+++ machines_filter.lua
@@ -9,6 +9,12 @@ file.close()
 return textutils.unserialize(data)
 end
 
+function save(table,name)
+local file = fs.open(name,"w")
+file.write(textutils.serialize(table))
+file.close()
+end
+
 function init()
 local slots = {}
 local i = 0
@@ -25,6 +31,7 @@ turtle.select(i)
 
 end
 
+save(slots, "machines_filter_db")
 
 return slots
 end
@@ -32,13 +39,18 @@ end
 --program begin
 
 local i = 0
+local i2 = 0
+local is_drop = false
 local items_count = 0
+local slots = {}
+
+rednet.open("right")
 
 --init
 if fs.exists("machines_filter_db") then
-local slots = load("machines_filter_db")
+slots = load("machines_filter_db")
 else
-local slots = init()
+slots = init()
 end
 --init end
 
@@ -47,14 +59,26 @@ for i = 1, 16, 1 do
 	turtle.select(i)
 	items_count = turtle.getItemCount(i)
 	
-	if slots["i"..i] then --было в фильтре
+	if slots["i"..tostring(i)] then --было в фильтре
 	
 		if items_count > 1 then
-			turtle.drop(items_count - 1)
+			turtle.dropDown(items_count - 1)
 		end
 	
 	else --не было в фильтре
-		turtle.dropUp(items_count)
+		
+		is_drop = false
+		
+		for i2 = 1, 16, 1 do
+		if slots["i"..tostring(i2)] and i2 ~= i then --было в фильтре
+			if turtle.compareTo(i2) then
+				turtle.dropDown(items_count)
+				is_drop = true
+			end
+		end
+		end
+
+		if not is_drop then turtle.dropUp(items_count) end
 	end
 end
 end
