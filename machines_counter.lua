19616a7e0321e60abf02eb75435a223447ca566d
shady2k <shady2k@gmail.com>
2013-02-04 23:18:44 +0400

    Update machines_counter.lua

--- machines_counter.lua
+++ machines_counter.lua
@@ -1,4 +1,4 @@
--- Program: Machines filter 0.1
+-- Program: Machines counter 0.1
 -- Author: shady2k
 -- Description: redpower item detector replace. Only for turtles. Sends data via rednet.
 
@@ -12,13 +12,31 @@ end
 
 server_id = 6478
 local i = 1
+local c = 2
 local items_count = 0
-is_stop = false
 
-while not is_stop do
+rednet.open("right")
+
+while 1 do
 items_count = 0
+
 turtle.select(i)
 items_count = turtle.getItemCount(i)
+
+if items_count ~= 0 then
+check_counter = 0
 turtle.drop()
 SendMessage({ count =  items_count})
+else
+
+if i == 1 then
+i = c
+c = c + 1
+if c > 16 then c = 2 end
+else
+i = 1
+end
+
+end
+
 end
