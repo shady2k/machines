cea47cc864c94662724a0398828f692eb6deb351
shady2k <shady2k@gmail.com>
2013-02-05 17:28:22 +0400

    Update machines_counter.lua

--- machines_counter.lua
+++ machines_counter.lua
@@ -8,14 +8,34 @@ rednet.send(server_id, msg);
 print("Send message: "..msg.."\n")
 end
 
---program begin
+function WaitForMessages()
 
-server_id = 6478
+while 1 do --while
+
+local event, param, data = os.pullEvent()
+
+if event == "rednet_message" and param == server_id then --rednet_message
+print("Receive message: "..data.."\n")
+
+msg = textutils.unserialize(data)
+
+if msg.action == "ping" then
+print("Send message: pong\n")
+rednet.send(server_id, {action = "ans", user_text = "pong"})
+end
+
+end --rednet_message
+
+end --while
+
+end
+
+
+function main()
+
 local i = 1
 local c = 2
 local items_count = 0
-
-rednet.open("right")
 
 while 1 do
 items_count = 0
@@ -26,7 +46,7 @@ items_count = turtle.getItemCount(i)
 if items_count ~= 0 then
 check_counter = 0
 turtle.drop()
-SendMessage({ count =  items_count})
+SendMessage({action = "count", count =  items_count})
 else
 
 if i == 1 then
@@ -39,4 +59,13 @@ end
 
 end
 
+end
+end
+
+--program begin
+server_id = 6478
+rednet.open("right")
+
+while 1 do
+parallel.waitForAll(function() main() end, function() WaitForMessages() end)
 end
